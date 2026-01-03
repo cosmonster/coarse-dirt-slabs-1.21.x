98d59be77389d4382fce2ba4868a230e0d016a4a
cosmonster <erika.dawiedofski@gmail.com>
2026-01-03 16:30:08 +0100

    Added "Test" comment.

--- src/main/java/csm/coarsedirtslab/CoarseDirtSlabs.java
+++ src/main/java/csm/coarsedirtslab/CoarseDirtSlabs.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//Test
 public class CoarseDirtSlabs implements ModInitializer {
 	public static final String MOD_ID = "coarsedirtslab";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
